5d25aab64fc4fa55d19d0982ea1a814b7e10b488
Iakov <kirbabin.jacob@gmail.com>
2025-08-04 03:50:21 +0300

    fast

--- components/Authorization/index.tsx
+++ components/Authorization/index.tsx
@@ -32,7 +32,7 @@ export const Authorization = () => {
 			password: pass,
 		};
 
-		const response = await fetch('http://localhost:3000/api/auth', {
+		const response = await fetch('/api/auth', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(bodyPost),
